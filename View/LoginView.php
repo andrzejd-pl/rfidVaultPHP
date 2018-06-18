e1d1d1524a44a4a8f99adc2c9cd92522f8612905
Andrzej Dybowski <dybowski@andrzejd.pl>
2018-06-18 22:57:20 +0200

    .

--- View/LoginView.php
+++ View/LoginView.php
@@ -31,6 +31,7 @@ class LoginView {
         <form action="/" method="post">
             Login: <input type="text" name="login" /><br />
             Password: <input type="password" name="password"><br />
+            <input type="submit" value="Log in" />
         </form>
         <?php
     }
