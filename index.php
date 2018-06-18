e8eb731ede63168d0a7e711131487efbe97cd0f9
Andrzej Dybowski <dybowski@andrzejd.pl>
2018-06-18 22:53:11 +0200

    .

--- index.php
+++ index.php
@@ -3,9 +3,10 @@ require_once './Controller/LoginController.php';
 
 session_start();
 
-if(isset($_SESSION['login']) && $_SESSION['login'] == true) {
+if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
     echo 'ok';
 }
 else {
     $controller = new \Controller\LoginController();
+    $controller->showForm();
 }
